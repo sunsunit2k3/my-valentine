04dc4c58a670a5a0ed83667966de12d6686b8827
SunSun <longcanh2k3@gmail.com>
2024-04-02 16:26:22 +0700

    new

---
+++ src/components/Home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <template v-if="showFirstTemplate">
+      <img :src="imageSrc" alt="">
+      <h1 class="text-4xl my-4 text-center">{{ question }}</h1>
+      <div class="choose">
+        <button class="yes" @click="handleYesClick">Yes</button>
+        <button class="no" @click="handleNoClick">{{ noButtonText }}</button>
+      </div>
+    </template>
+    <template v-else>
+      <img :src="newImageSrc" alt="">
+      <h1 class="text-4xl my-4 text-center">{{ newQuestion }}</h1>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.yes {
+  background-color: green;
+  color: white;
+  padding: 10px 20px;
+  margin: 10px;
+  border-radius: 5px;
+}
+
+.no {
+  background-color: red;
+  color: white;
+  padding: 10px 20px;
+  margin: 10px;
+  border-radius: 5px;
+}
+
+.choose {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  width: 300px;
+  height: 300px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      question: "Will you be my Valentine?",
+      newQuestion: "I Love You",
+      newImageSrc:"https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif",
+      imageSrc: "https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif",
+      noButtonText: "No",
+      listNoButtonText: ["No", "Are You Sure?", "Really sure?", "Are you positive???", 
+      "Pookie please", 'Just think about it', "If you say no, I'll be very sad", 
+      "I'll be very very sad", "I'll be very very very sad", "I'll be very very very very sad",
+      "Ok fine, I'll stop asking...","Just kidding, PLEASE SAY YES","You're breaking my heart ;("],
+      noButtonIndex: 0,
+      showFirstTemplate: true,
+    }
+  },
+  methods: {
+    handleYesClick() {
+      this.showFirstTemplate = false;
+    },
+    handleNoClick() {
+      // Replace the "No" button text with a different word when "No" button is clicked
+      this.noButtonIndex = (this.noButtonIndex + 1) % this.listNoButtonText.length;
+      this.noButtonText = this.listNoButtonText[this.noButtonIndex];
+    }
+  }
+}
+</script>
