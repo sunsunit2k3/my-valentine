d3c0a2e1977a3c632eff9ecccb5c2b5c6b0d635a
SunSun <longcanh2k3@gmail.com>
2024-04-03 10:22:31 +0700

    new

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,11 @@
     <template v-if="showFirstTemplate">
       <img :src="imageSrc" alt="">
       <h1 class="text-4xl my-4 text-center">{{ question }}</h1>
-      <div class="choose">
-        <button class="yes" @click="handleYesClick">Yes</button>
-        <button class="no" @click="handleNoClick">{{ noButtonText }}</button>
+      <div class="flex flex-wrap flex-col md:flex-row gap-4 items-center justify-center">
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" @click="handleYesClick"
+          :style="{ 'font-size': yesFontSize }">Yes</button>
+        <button class=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" @click="handleNoClick">{{
+      noButtonText }}</button>
       </div>
     </template>
     <template v-else>
@@ -14,52 +16,27 @@
     </template>
   </div>
 </template>
-
-<style scoped>
-.yes {
-  background-color: green;
-  color: white;
-  padding: 10px 20px;
-  margin: 10px;
-  border-radius: 5px;
-}
-
-.no {
-  background-color: red;
-  color: white;
-  padding: 10px 20px;
-  margin: 10px;
-  border-radius: 5px;
-}
-
-.choose {
-  display: flex;
-  justify-content: center;
-}
-
-img {
-  width: 300px;
-  height: 300px;
-}
-</style>
+<style src="@assets/style.css"></style>
 
 <script>
-import image2 from"@assets/bear_2.gif";
+import image2 from "@assets/bear_2.gif";
 import image1 from "@assets/bear_1.gif";
 export default {
   data() {
     return {
       question: "Will you be my Valentine?",
       newQuestion: "I Love You",
-      newImageSrc:image2,
+      newImageSrc: image2,
       imageSrc: image1,
       noButtonText: "No",
-      listNoButtonText: ["No", "Are You Sure?", "Really sure?", "Are you positive???", 
-      "Pookie please", 'Just think about it', "If you say no, I'll be very sad", 
-      "I'll be very very sad", "I'll be very very very sad", "I'll be very very very very sad",
-      "Ok fine, I'll stop asking...","Just kidding, PLEASE SAY YES","You're breaking my heart ;("],
+      listNoButtonText: ["No", "Are You Sure?", "Really sure?", "Are you positive???",
+        "Pookie please", 'Just think about it', "If you say no, I'll be very sad",
+        "I'll be very very sad", "I'll be very very very sad", "I'll be very very very very sad",
+        "Ok fine, I'll stop asking...", "Just kidding, PLEASE SAY YES", "You're breaking my heart ;("],
       noButtonIndex: 0,
       showFirstTemplate: true,
+      yesFontSize: "16px",
+      x: 16,
     }
   },
   methods: {
@@ -67,9 +44,11 @@ export default {
       this.showFirstTemplate = false;
     },
     handleNoClick() {
-      // Replace the "No" button text with a different word when "No" button is clicked
+      this.x = this.x +16;
       this.noButtonIndex = (this.noButtonIndex + 1) % this.listNoButtonText.length;
       this.noButtonText = this.listNoButtonText[this.noButtonIndex];
+      this.yesFontSize =  this.x + "px";;
+      console.log(this.x)
     }
   }
 }
